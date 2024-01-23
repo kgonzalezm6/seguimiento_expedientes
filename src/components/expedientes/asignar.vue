5b7298164dffbf49963f414376f6c90d3a50e6ab
krgonzalez <krgonzalez@muniguate.com>
2024-01-23 08:30:00 -0600

    pryeba

---
+++ src/components/expedientes/asignar.vue
@@ -0,0 +1,26 @@
+<script setup>
+    import { onBeforeMount } from 'vue';
+import { useGlobalStore } from '../../stores/global';
+import { useexpedientesStore } from '../../stores/expedientes';
+const globalStore = useGlobalStore();
+const expedienteStore = useexpedientesStore();
+</script>
+
+<template>
+    <div>
+        <modal :open="expedienteStore.open_asignar" title="Asignar expediente" icon="user-check" @close="expedienteStore.open_asignar= !expedienteStore.open_asignar">
+
+            {{ expedienteStore.one_expediente.id }}
+            <fieldset>
+                <legend>Asignar</legend>    
+                <v-select />
+            </fieldset>
+
+            <template #footer>
+                <div class="flex ml-auto">
+                   <btn text="Asignar" class="bg-green-500 text-white" icon="save"/> 
+                </div>
+            </template>
+        </modal>
+    </div>
+</template>
